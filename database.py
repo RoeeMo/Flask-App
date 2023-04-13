fb46b1c57c844df13020fa36bcc0e0294a61bab8
Roee Moreno <roee.moreno@hubsecurity.io>
2023-04-13 21:26:20 +0300

    Changed the items from being saved in a dictionary of lists to a list of dictionaries for more readable access

--- database.py
+++ database.py
@@ -7,16 +7,13 @@ engine = create_engine(os.getenv("db_conn_str"),
                        connect_args={"ssl": {"ssl_ca": "/etc/ssl/cert.pem"}}
                        )
 
-# Extract all items from DB and stores them in a dictionary
+# Extract all items and returns a list of dictionaries (each item in a seperate dictionary)
 def load_items():
   with engine.connect() as conn:
     result = conn.execute(text("select * from items"))
 
-  result_dict = {"name": [], "price": [], "image_url": []}
-
+  items = []
   for row in result.all():
-    result_dict.setdefault("name", []).append(row[1])
-    result_dict.setdefault("price", []).append(str(row[2]) + row[3])
-    result_dict.setdefault("image_url", []).append(row[4])
-
-  return(result_dict)
+    item_dict = {"name": row[1], "price": str(row[2]) + row[3], "image_url": row[4]}
+    items.append(item_dict)
+  return(items)
