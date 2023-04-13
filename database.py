b664758aaa54abe04eeb94988dcb762846b19690
Roee Moreno <72334269+RoeeMo@users.noreply.github.com>
2023-04-13 19:50:51 +0000

    Fixed some bugs

--- database.py
+++ database.py
@@ -1,11 +1,11 @@
 from sqlalchemy import create_engine, text
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import os
-load_dotenv()
+# load_dotenv()
 
-engine = create_engine(os.getenv("db_conn_str"), 
-                       connect_args={"ssl": {"ssl_ca": "/etc/ssl/cert.pem"}}
-                       )
+db_conn_str = os.environ['DB_CONN_STR']
+engine = create_engine(db_conn_str, connect_args={"ssl": {"ssl_ca": ""}})
+
 
 # Extract all items and returns a list of dictionaries (each item in a seperate dictionary)
 def load_items():
@@ -14,6 +14,10 @@ def load_items():
 
   items = []
   for row in result.all():
-    item_dict = {"name": row[1], "price": str(row[2]) + row[3], "image_url": row[4]}
+    item_dict = {
+      "name": row[1],
+      "price": str(row[2]) + row[3],
+      "image_url": row[4]
+    }
     items.append(item_dict)
-  return(items)
+  return (items)
