a016b5f5d83bc6806bab011773fdb70c175e78c9
RoeeMo <72334269+RoeeMo@users.noreply.github.com>
2023-04-18 05:53:15 +0000

    Added authorization checks

--- app.py
+++ app.py
@@ -1,36 +1,105 @@
-from flask import Flask, render_template, jsonify, request
-from database import load_items, add_item_from_db
+from flask import Flask, render_template, url_for, jsonify, request, flash, make_response, redirect
+from database import load_items, add_item_to_db, register_user, search_db, authenticate, del_item_from_db
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'somesecretkeyfordev'
+permsObj = {'user': ['view', 'edit']}
 
-@app.route("/")
+
+@app.route('/')
 def home():
   items = load_items()
-  return render_template('index.html', items=items)
+  return render_template('index.html',
+                         items=items,
+                         username=is_authorized(
+                           request.cookies.get('sess_cookie')))
 
 
-@app.route("/register")
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-  return render_template("/register")
+  if request.method == 'POST':
+    username = request.form.get('username')
+    password1 = request.form.get('password1')
+    password2 = request.form.get('password2')
+
+    if len(username) < 4:
+      flash('Username need to be longer than 4 characters', category='error')
+    elif password1 != password2:
+      flash('Passwords don\'t match', category='error')
+    elif len(password1) < 7:
+      flash('Password must be atleast 7 characters', category='error')
+    else:
+      res = register_user(username, password1)
+      if 'exist' in res:
+        flash(res, category='error')
+        return render_template('register.html')
+      return redirect(url_for('home'))
+    return render_template('register.html')
+  return render_template('register.html')
 
 
-@app.route("/login")
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-  return render_template("/login")
+  if request.method == 'GET':
+    return render_template('login.html')
+  else:
+    data = request.form
+    cookie = authenticate(data.get('username'), data.get('password'))
+    if cookie:
+      resp = make_response(redirect('/'))
+      resp.set_cookie('sess_cookie', cookie)
+      return resp
+
+    flash('Username Or Password Was Incorrect', category='error')
+    return render_template('login.html')
 
 
-@app.route("/add_item", methods=['post'])
+@app.route('/add_item', methods=['POST'])
 def add_item():
-  data = request.form
-  add_item_from_db(data)
-  return jsonify(data)
+  if is_authorized(request.cookies.get('sess_cookie'), 'add'):
+    data = request.form
+    add_item_to_db(data)
+    resp = make_response(redirect('/'))
+    return resp
+  flash('Not Enough Permissions', category='error')
+  resp = make_response(redirect('/'))
+  return resp
 
 
-@app.route("/api/items")
+# @app.route('/edit_item', methods=['POST'])
+# def edit_item():
+#   if is_authorized(request.cookies.get('sess_cookie'), 'edit'):
+#     data = request.form
+
+
+@app.route('/del_item', methods=['POST'])
+def del_item():
+  if is_authorized(request.cookies.get('sess_cookie'), 'del'):
+    del_item_from_db(request.form.get('item_id'))
+    resp = make_response(redirect('/'))
+    return resp
+  flash('Not Enough Permissions', category='error')
+  resp = make_response(redirect('/'))
+  return resp
+
+
+@app.route('/api/items')
 def list_items():
   return jsonify(load_items())
 
 
-if __name__ == "__main__":
+def is_authorized(cookie, action="null"):
+  try:
+    type = search_db('users', 'cookie', cookie, grep='type')[0][0]
+    user = search_db('users', 'cookie', cookie, grep='username')[0][0]
+    if (type == 'admin') or (action in permsObj[type]):
+      return user
+    else:
+      return False
+  except:
+    return
+
+
+if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,6 +1,8 @@
-from sqlalchemy import create_engine, text
 import os
+from sqlalchemy import create_engine, text
+import hashlib, secrets
 
+my_secret = os.environ['PASS_SALT']
 db_conn_str = os.environ['DB_CONN_STR']
 engine = create_engine(db_conn_str, connect_args={"ssl": {"ssl_ca": ""}})
 
@@ -13,6 +15,7 @@ def load_items():
   items = []
   for row in result.all():
     item_dict = {
+      "id": row[0],
       "name": row[1],
       "price": str(row[2]) + row[3],
       "image_url": row[4],
@@ -22,8 +25,7 @@ def load_items():
   return (items)
 
 
-def add_item_from_db(item):
-
+def add_item_to_db(item):
   with engine.connect() as conn:
     query = text(
       "INSERT INTO items (name, price, currency, image_url, description) VALUES (:name, :price, :currency, :image_url, :description)"
@@ -35,3 +37,68 @@ def add_item_from_db(item):
                              description=item['description'])
 
     conn.execute(query)
+
+
+# def edit_item_from_db(item):
+#   with engine.connect() as conn:
+#     result = conn.execute(
+#       text("DELETE FROM items WHERE (id = :id)").bindparams(id=item))
+#     return
+
+
+def del_item_from_db(item):
+  with engine.connect() as conn:
+    result = conn.execute(
+      text("DELETE FROM items WHERE (id = :id)").bindparams(id=item))
+    return
+
+
+def search_db(table, column, params, grep='*', validate_caps=True):
+  tmp = ''
+  if validate_caps:
+    tmp = 'BINARY'
+  with engine.connect() as conn:
+    query = text(f"SELECT {grep} FROM {table} WHERE {tmp} {column} = :params")
+    query = query.bindparams(params=params)
+    result = conn.execute(query)
+  return result.all()
+
+
+def register_user(username, password):
+  with engine.connect() as conn:
+    query = text("SELECT * FROM users WHERE username = :username")
+    query = query.bindparams(username=username)
+    result = conn.execute(query)
+    existing_user = result.fetchone()
+    if existing_user:
+      return 'User already exist!'
+    else:
+      hashed_string = hashlib.sha512(my_secret.encode() +
+                                     password.encode()).hexdigest()
+      query = text(
+        "INSERT INTO users (username, password, type) VALUES (:username, :password, 'user')"
+      ).bindparams(username=username, password=hashed_string)
+
+      result = conn.execute(query)
+      return 'User created successfully'
+
+
+def authenticate(username, password):
+  with engine.connect() as conn:
+    hashed_string = hashlib.sha512(my_secret.encode() +
+                                   password.encode()).hexdigest()
+    query = text(
+      "SELECT * FROM users WHERE BINARY username = :username AND BINARY password = :password"
+    )
+    query = query.bindparams(username=username, password=hashed_string)
+    result = conn.execute(query)
+    try:
+      a = result.all()[0]
+      cookie = secrets.token_hex(16)
+      query = text(
+        "UPDATE users SET cookie = :cookie WHERE username = :username")
+      query = query.bindparams(cookie=cookie, username=username)
+      conn.execute(query)
+      return cookie
+    except:
+      return ''
